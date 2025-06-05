349eb2ba4a1f74862a2360e67144c71ef8bf20eb
AlbayEmre <YunsEmreAlbayrak@gmail.com>
2025-06-05 23:16:07 +0300

    deneme

---
+++ dummy_c.c
@@ -0,0 +1,4 @@
+// dummy_wrapper.c
+int main() {
+    return 0;  // sadece Coverity'nin bir build yakalaması için
+}
--- example_project.py
+++ example_project.py
@@ -1,40 +1,67 @@
-# example_project.py
+import os
 
-def unused_variable():
-    a = 42  # Kullanılmayan değişken
-    return True
-
-def divide_by_zero():
-    b = 1 / 0  # Sıfıra bölme hatası
-
-def index_error():
-    arr = [1, 2, 3]
-    return arr[5]  # IndexError
-
-def type_error():
-    c = 'abc' + 10  # TypeError
-
-def file_open():
-    f = open("file.txt", "r")  # Dosya kapatılmıyor
-
-def syntax_error()
-    print("Eksik parantez")  # Syntax hatası
-
-def always_false():
+def unused_variables_and_dead_code():
+    x = 100  # Kullanılmayan değişken
+    y = 0
     if False:
-        return "Should never happen"
-    else:
-        return
+        print("Asla çalışmayacak kod")
 
-def no_exception_handling():
-    return int("not a number")  # ValueError, yakalanmıyor
+def division_by_zero_handling():
+    try:
+        result = 10 / 0  # Sıfıra bölme
+    except ZeroDivisionError as e:
+        print(f"Hata: {e}")
+
+def file_operations_with_leak():
+    f = open("example.txt", "w")  # Dosya kapatılmıyor
+    f.write("Merhaba dünya")  # Dosya düzgün kapatılmadığı için sorun olabilir
+
+def unsafe_type_operations():
+    value = "123"
+    result = value + 10  # TypeError
+
+def unsafe_index_access():
+    lst = [1, 2, 3]
+    return lst[10]  # IndexError
+
+def unsafe_conversion():
+    num = int("oniki")  # ValueError
+
+def poor_exception_handling():
+    try:
+        risky = [][2]
+    except:
+        pass  # Kötü uygulama: ne hata olduğu bilinmiyor
+
+def missing_import_usage():
+    # math modülünü import etmedik
+    return math.sqrt(16)  # NameError
+
+def infinite_loop():
+    while True:
+        break  # Sadece örnek, gerçek sonsuz döngü olmaması için break
+
+def logic_bug():
+    is_logged_in = False
+    if is_logged_in:
+        return "Hoş geldiniz!"
+    else:
+        return "Giriş yapıldı"  # Mantıksal hata: yanlış mesaj
+
+def no_return_function():
+    a = 10 + 5  # Değer dönmüyor
+
+# syntax error: print("Eksik parantez"  # bu yorum satırında
 
 if __name__ == "__main__":
-    unused_variable()
-    divide_by_zero()
-    index_error()
-    type_error()
-    file_open()
-    syntax_error()
-    always_false()
-    no_exception_handling()
+    unused_variables_and_dead_code()
+    division_by_zero_handling()
+    file_operations_with_leak()
+    unsafe_type_operations()
+    unsafe_index_access()
+    unsafe_conversion()
+    poor_exception_handling()
+    missing_import_usage()
+    infinite_loop()
+    logic_bug()
+    no_return_function()
